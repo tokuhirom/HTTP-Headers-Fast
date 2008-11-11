8df7a716f4c394fa39f32bfe72a0842c74952c32
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-11-11 02:08:39 +0000

    added more benchmark cases
    git-svn-id: http://svn.coderepos.org/share/lang/perl/HTTP-Headers-Fast/trunk@23184 d0d07461-0603-4401-acd4-de1884942a52

--- tools/benchmark.pl
+++ tools/benchmark.pl
@@ -4,7 +4,59 @@ use Benchmark qw/cmpthese/;
 use HTTP::Headers;
 use HTTP::Headers::Fast;
 
+my %source = (
+    'Connection'     => 'close',
+    'Date'           => 'Tue, 11 Nov 2008 01:16:37 GMT',
+    'Content-Length' => 3744,
+    'Content-Type'   => 'text/html',
+    'Status'         => 200,
+);
+
 my @cases = (
+    push_header => sub {
+        my $h = HTTP::Headers->new;
+        my $f = HTTP::Headers::Fast->new;
+
+        cmpthese(
+            1000000 => {
+                orig => sub { $h->push_header('X-Foo' => 1) },
+                fast => sub { $f->push_header('X-Foo' => 1) },
+            },
+        );
+    },
+    scan => sub {
+        my $h = HTTP::Headers->new(%source);
+        my $f = HTTP::Headers::Fast->new(%source);
+
+        cmpthese(
+            1000000 => {
+                orig => sub { $h->scan(sub { }) },
+                fast => sub { $f->scan(sub { }) },
+            },
+        );
+    },
+    get_header => sub {
+        my $h = HTTP::Headers->new;
+        my $f = HTTP::Headers::Fast->new;
+
+        cmpthese(
+            1000000 => {
+                orig => sub { $h->header('Content-Length') },
+                fast => sub { $f->header('Content-Length') },
+            },
+        );
+    },
+    set_header => sub {
+        my $h = HTTP::Headers->new;
+        my $f = HTTP::Headers::Fast->new;
+
+        cmpthese(
+            1000000 => {
+                orig => sub { $h->header('Content-Length' => 100) },
+                fast => sub { $f->header('Content-Length' => 100) },
+            },
+        );
+    },
     get_content_length => sub {
         my $h = HTTP::Headers->new;
         my $f = HTTP::Headers::Fast->new;
@@ -17,8 +69,8 @@ my @cases = (
         );
     },
     as_string => sub {
-        my $h = HTTP::Headers->new(foo => "bar", foo => "baaaaz", Foo => "baz");
-        my $f = HTTP::Headers::Fast->new(foo => "bar", foo => "baaaaz", Foo => "baz");
+        my $h = HTTP::Headers->new(%source);
+        my $f = HTTP::Headers::Fast->new(%source);
         cmpthese(
             100000 => {
                 orig => sub { $h->as_string },
