bffdf93aa575b0b400a277083187a6b7487998cc
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-11-11 01:04:27 +0000

    refactor the benchmark script
    git-svn-id: http://svn.coderepos.org/share/lang/perl/HTTP-Headers-Fast/trunk@23179 d0d07461-0603-4401-acd4-de1884942a52

--- tools/benchmark.pl
+++ tools/benchmark.pl
@@ -4,12 +4,34 @@ use Benchmark qw/cmpthese/;
 use HTTP::Headers;
 use HTTP::Headers::Fast;
 
-my $h = HTTP::Headers->new;
-my $f = HTTP::Headers::Fast->new;
+my @cases = (
+    get_content_length => sub {
+        my $h = HTTP::Headers->new;
+        my $f = HTTP::Headers::Fast->new;
 
-cmpthese(
-    1000000 => {
-        orig => sub { $h->content_length },
-        fast => sub { $f->content_length },
+        cmpthese(
+            1000000 => {
+                orig => sub { $h->content_length },
+                fast => sub { $f->content_length },
+            },
+        );
+    },
+    as_string => sub {
+        my $h = HTTP::Headers->new(foo => "bar", foo => "baaaaz", Foo => "baz");
+        my $f = HTTP::Headers::Fast->new(foo => "bar", foo => "baaaaz", Foo => "baz");
+        cmpthese(
+            10000 => {
+                orig => sub { $h->as_string },
+                fast => sub { $f->as_string },
+            },
+        );
     },
 );
+my $only = shift @ARGV;
+while (my ($name, $code) = splice(@cases, 0, 2)) {
+    next if $only && $only ne $name;
+    print "-- $name\n";
+    $code->();
+    print "\n\n";
+}
+
--- tools/prof.pl
+++ tools/prof.pl
@@ -2,6 +2,6 @@ use strict;
 use warnings;
 use HTTP::Headers::Fast;
 
-my $f = HTTP::Headers::Fast->new;
+my $f = HTTP::Headers::Fast->new(foo => 'bar', baz => 'see', 'moo' => 'yan');
 
-for (0..100) { $f->content_length }
+for (0..100) { $f->as_string }
