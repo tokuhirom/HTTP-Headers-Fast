624b9c2c36914827305b2f9c74fd0da2cc2b9804
Mark Stosberg <mark@summersault.com>
2010-01-22 12:57:12 -0500

    Strip out empty lines separated by CRLF.     Before the patch, an embedded CRLF would be allowed through with a     space added after it. This is spec-compliant but appears not to be     the intent, based on the regex to remove empty lines in headers.  If     a client accepted "CRLF\sCRLF\s" as a valid header end, then a CRLF     injection attack would have been possible.     (This patch was ported from the HTTP::Headers git repo. )
        The possiblity of the former behavior exploitably seems particular small. Apache
        won't even send out header that a space between the two required CRLFs.

--- lib/HTTP/Headers/Fast.pm
+++ lib/HTTP/Headers/Fast.pm
@@ -319,7 +319,7 @@ sub _process_newline {
     my $endl = shift;
     # must handle header values with embedded newlines with care
     s/\s+$//;        # trailing newlines and space must go
-    s/\n\n+/\n/g;    # no empty lines
+    s/\n(\x0d?\n)+/\n/g;     # no empty lines
     s/\n([^\040\t])/\n $1/g; # intial space for continuation
     s/\n/$endl/g;    # substitute with requested line ending
     $_;
